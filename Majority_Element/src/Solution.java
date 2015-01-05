1e4827562954de539a7163748515102b582e9cc9
panqiuhui <qiuhui.pan@gmail.com>
2015-01-05 21:23:24 +0800

    Merge branch 'master' of https://github.com/panqiuhui/leetcode
    Conflicts:
    	README.md

---
+++ Majority_Element/src/Solution.java
@@ -0,0 +1,27 @@
+//与编程之美中“寻找发帖水王”问题本质相同
+public class Solution {
+    public int majorityElement(int[] num) {
+        int result = 0;
+        int times = 0;
+        for (int i = 0; i < num.length; i++) {
+            if (times == 0) {
+                result = num[i];
+                times++;
+            } else {
+                if (result != num[i]) {
+                    times--;
+                } else {
+                    times++;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] num = {4,5,4};
+        int result = s.majorityElement(num);
+        System.out.println(result);
+    }
+}
